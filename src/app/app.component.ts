b6f05376dba4877e2200c83ce6572cdb85cd5e98
ejetzael <ejetzael@gmail.com>
2020-02-22 14:20:08 -0600

    cambio en el nombre de package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'academind';
 
-  opened: false;
+  opened:any = false;
 
   constructor(){}
 
